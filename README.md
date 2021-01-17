75d70ed748ce2a53fefbf568bfaf54c6723e8ef5
thanhtunng <thanhtungvu.hust@gmail.com>
2021-01-17 17:26:45 +0700

    Add parse argument option
      - Add parse args action
      - Mod print_elapsed to elapsed for convinience
      - Update modules in requirement.txt
    
    Signed-off-by: thanhtunng <thanhtungvu.hust@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fsdelta
 A tool to simulate swupdate
+
+# Usage
+
+- Install requirement:
+    ```
+    pip3 install -r requirement.txt
+    ```
+
+- Create tar file:
+    ```
+    python3 create_file.py
+    ```
--- create_tar.py
+++ create_tar.py
@@ -3,26 +3,41 @@ import tarfile
 from alive_progress import alive_bar
 import timeit
 import time
+from progress.spinner import MoonSpinner
+import argparse
 
 diff_list = ["/thanhtungvu/ICT/term9/Internetworking_note.txt", "/thanhtungvu/ICT/term9/note_internet_working",
              "/thanhtungvu/ICT/term9/NetworkSecurity/week7/"]
 
 def createTar(tar_name):
     with tarfile.open(tar_name, "w:gz") as tar_obj:
-        for obj in diff_list:
-            tar_obj.add("/home"+obj,arcname=obj)
-            yield
+        with alive_bar(700, bar="bubbles") as bar:
+            for obj in diff_list:
+                tar_obj.add("/home"+obj,arcname=obj)
+                bar()
 
-def print_elapsed_time(prefix=''):
+def elapsed_time(prefix=''):
     e_time = time.time()
-    if not hasattr(print_elapsed_time, 's_time'):
-        print_elapsed_time.s_time = e_time
+    if not hasattr(elapsed_time, 's_time'):
+        elapsed_time.s_time = e_time
     else:
-        print(f'{prefix} consumes: {e_time - print_elapsed_time.s_time:.2f} sec')
-        print_elapsed_time.s_time = e_time
+        print(f'{prefix} used: {e_time - elapsed_time.s_time:.2f} sec')
+        elapsed_time.s_time = e_time
 
-print_elapsed_time()
-with alive_bar(700, bar="bubbles") as bar:
-    for i in createTar("delta.tar.gz"):
-        bar()
-print_elapsed_time("Creating delta.tar.gz")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", dest="filename",
+                    help="write report to FILE", metavar="FILE")
+parser.add_argument("-q", "--quiet",
+                    action="store_false", dest="verbose", default=True,
+                    help="don't print status messages to stdout")
+
+args = parser.parse_args()
+
+elapsed_time()
+# with alive_bar(700, bar="bubbles") as bar:
+#     for i in createTar("delta.tar.gz"):
+#         bar()
+createTar("delta.tar.gz")
+elapsed_time("Totaltime ")
+
